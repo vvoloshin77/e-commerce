74de73e9e6f434f8eabadfc6a39493fd2becd027
kimikosi77 <usgeru@yandex.ru>
2026-05-07 21:39:24 +0300

    added new func in class

--- src/product_category.py
+++ src/product_category.py
@@ -14,6 +14,24 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, prod_dic: dict, products_list: list):
+        """Класс-метод сравнивающий товары по названию и складывающий остатки вместе, устанавливаю большую цену"""
+        for product in products_list:
+            if product.name == prod_dic['name']:
+                product.quantity += prod_dic['quantity']
+
+                if prod_dic['price'] > product.price:
+                    product.price = prod_dic['price']
+                return product
+
+        return cls(
+            prod_dic['name'],
+            prod_dic['description'],
+            prod_dic['price'],
+            prod_dic['quantity']
+        )
+
 
 class Category:
     """Класс содержащий информацию о категории"""
@@ -26,10 +44,20 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
-        Category.products_count += len(self.products)
+        self.__products = products
+        Category.products_count += len(self.__products)
         Category.category_count += 1
 
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.products_count += 1
+
+    @property
+    def products(self):
+        product_str = ''
+        for product in self.__products:
+            product_str += f'\n{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return product_str
 
 if __name__ == '__main__':
     with open(r'C:\Users\usger\PycharmProjects\e_commerce\data\products.json', 'r', encoding='UTF-8') as f:
